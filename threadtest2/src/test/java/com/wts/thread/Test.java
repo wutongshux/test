6f1b49b0360f6884a888de37263832a14038a4b0
wutongshux <719668370@qq.com>
2018-06-25 18:27:38 +0800

    提交

---
+++ threadtest2/src/test/java/com/wts/thread/Test.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.wts.thread;
+
+
+    /** 
+    * @ClassName: Test 
+    * @Description: TODO(这里用一句话描述这个类的作用) 
+    * @author wutongshu
+    * @date 2018年6月25日 下午6:13:06 
+    */
+
+public class Test {
+	public static void main(String[] args) {
+		System.out.println("hello");
+	}
+}
